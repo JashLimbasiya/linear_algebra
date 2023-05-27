c5ba0fe28d26cd4f41d8d708ad1f526b12956332
JashLimbasiya11 <89210135+JashLimbasiya@users.noreply.github.com>
2023-05-27 19:17:06 +0530

    27_05_2023

--- lib/screens/vector_spaces/orthogonal_vectors/orthogonal_vectors.dart
+++ lib/screens/vector_spaces/orthogonal_vectors/orthogonal_vectors.dart
@@ -8,6 +8,13 @@ class OrthogonalVectors extends StatefulWidget {
   State<OrthogonalVectors> createState() => _OrthogonalVectorsState();
 }
 
+TextEditingController vectora1 = TextEditingController();
+TextEditingController vectora2 = TextEditingController();
+TextEditingController vectora3 = TextEditingController();
+TextEditingController vectorb1 = TextEditingController();
+TextEditingController vectorb2 = TextEditingController();
+TextEditingController vectorb3 = TextEditingController();
+
 class _OrthogonalVectorsState extends State<OrthogonalVectors> {
   @override
   Widget build(BuildContext context) {
@@ -16,11 +23,339 @@ class _OrthogonalVectorsState extends State<OrthogonalVectors> {
         appBar: appbar("Orthogonal Vectors"),
         body: Container(
           decoration: background(),
-          child: const Center(
-              child: Text("Orthogonal Vectors",
-                  style: TextStyle(fontSize: 50, color: Colors.red))),
+          child: ListView(
+            padding:
+                const EdgeInsets.only(top: 25, right: 10, bottom: 10, left: 10),
+            children: [
+              Card(
+                  shadowColor: Colors.red, elevation: 10, child: vectorColumn())
+            ],
+          ),
         ),
       ),
     );
   }
+
+  vectorColumn() {
+    switch (dimflag) {
+      case 1:
+        return Column(
+          children: [
+            const SizedBox(height: 10),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text(
+                "Select Dimension : ",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(width: 5),
+              redTextButton("1D", () {
+                setState(() {
+                  dimflag = 1;
+                  ans = "";
+                  _OrthogonalVectorsState();
+                });
+              }),
+              const SizedBox(width: 10),
+              whiteTextButton("2D", () {
+                setState(() {
+                  dimflag = 2;
+                  ans = "";
+                  _OrthogonalVectorsState();
+                });
+              }),
+              const SizedBox(width: 10),
+              whiteTextButton("3D", () {
+                setState(() {
+                  dimflag = 3;
+                  ans = "";
+                  _OrthogonalVectorsState();
+                });
+              })
+            ]),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Vector A : ",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(width: 15),
+                SizedBox(
+                  child: texInput(100, 15, vectora1, "a1"),
+                )
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Vector B : ",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(width: 15),
+                SizedBox(
+                  child: texInput(100, 15, vectorb1, "b1"),
+                )
+              ],
+            ),
+            const SizedBox(height: 25),
+            whiteTextButton('Calculate', () {
+              setState(() {
+                a1 = double.parse(vectora1.text);
+                b1 = double.parse(vectorb1.text);
+                dot = (a1 * b1);
+                if (dot == 0) {
+                  ans = "True";
+                } else {
+                  ans = "False";
+                }
+                vectora1.clear();
+                vectorb1.clear();
+                _OrthogonalVectorsState();
+              });
+            }),
+            const SizedBox(height: 25),
+            const Divider(
+              color: Color.fromARGB(70, 200, 0, 0),
+              thickness: 3,
+              indent: 5,
+              endIndent: 5,
+            ),
+            const SizedBox(height: 10),
+            ansRow("Orthogonal : $ans"),
+            const SizedBox(height: 20),
+          ],
+        );
+
+      case 2:
+        return Column(
+          children: [
+            const SizedBox(height: 10),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text(
+                "Select Dimension : ",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(width: 5),
+              whiteTextButton("1D", () {
+                setState(() {
+                  dimflag = 1;
+                  ans = "";
+                  _OrthogonalVectorsState();
+                });
+              }),
+              const SizedBox(width: 10),
+              redTextButton("2D", () {
+                setState(() {
+                  dimflag = 2;
+                  ans = "";
+                  _OrthogonalVectorsState();
+                });
+              }),
+              const SizedBox(width: 10),
+              whiteTextButton("3D", () {
+                setState(() {
+                  dimflag = 3;
+                  ans = "";
+                  _OrthogonalVectorsState();
+                });
+              })
+            ]),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Vector A : ",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(width: 15),
+                SizedBox(
+                  child: texInput(80, 15, vectora1, "a1"),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  child: texInput(80, 15, vectora2, "a2"),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Vector B : ",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(width: 15),
+                SizedBox(
+                  child: texInput(80, 15, vectorb1, "b1"),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  child: texInput(80, 15, vectorb2, "b2"),
+                ),
+              ],
+            ),
+            const SizedBox(height: 25),
+            whiteTextButton('Calculate', () {
+              setState(() {
+                a1 = double.parse(vectora1.text);
+                a2 = double.parse(vectora2.text);
+                b1 = double.parse(vectorb1.text);
+                b2 = double.parse(vectorb2.text);
+                dot = (a1 * b1) + (a2 * b2);
+                if (dot == 0) {
+                  ans = "True";
+                } else {
+                  ans = "False";
+                }
+                vectora1.clear();
+                vectora2.clear();
+                vectorb1.clear();
+                vectorb2.clear();
+                _OrthogonalVectorsState();
+              });
+            }),
+            const SizedBox(height: 25),
+            const Divider(
+              color: Color.fromARGB(70, 200, 0, 0),
+              thickness: 3,
+              indent: 5,
+              endIndent: 5,
+            ),
+            const SizedBox(height: 10),
+            ansRow("Orthogonal : $ans"),
+            const SizedBox(height: 20),
+          ],
+        );
+
+      case 3:
+        return Column(
+          children: [
+            const SizedBox(height: 10),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text(
+                "Select Dimension : ",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(width: 5),
+              whiteTextButton("1D", () {
+                setState(() {
+                  dimflag = 1;
+                  ans = "";
+                  _OrthogonalVectorsState();
+                });
+              }),
+              const SizedBox(width: 10),
+              whiteTextButton("2D", () {
+                setState(() {
+                  dimflag = 2;
+                  ans = "";
+                  _OrthogonalVectorsState();
+                });
+              }),
+              const SizedBox(width: 10),
+              redTextButton("3D", () {
+                setState(() {
+                  dimflag = 3;
+                  ans = "";
+                  _OrthogonalVectorsState();
+                });
+              })
+            ]),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Vector A : ",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(width: 15),
+                SizedBox(
+                  child: texInput(60, 15, vectora1, "a1"),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  child: texInput(60, 15, vectora2, "a2"),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  child: texInput(60, 15, vectora3, "a3"),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Vector B : ",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(width: 15),
+                SizedBox(
+                  child: texInput(60, 15, vectorb1, "b1"),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  child: texInput(60, 15, vectorb2, "b2"),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  child: texInput(60, 15, vectorb3, "b2"),
+                ),
+              ],
+            ),
+            const SizedBox(height: 25),
+            whiteTextButton('Calculate', () {
+              setState(() {
+                a1 = double.parse(vectora1.text);
+                a2 = double.parse(vectora2.text);
+                a3 = double.parse(vectora3.text);
+                b1 = double.parse(vectorb1.text);
+                b2 = double.parse(vectorb2.text);
+                b3 = double.parse(vectorb3.text);
+                dot = (a1 * b1) + (a2 * b2) + (a3 * b3);
+                if (dot == 0) {
+                  ans = "True";
+                } else {
+                  ans = "False";
+                }
+                vectora1.clear();
+                vectora2.clear();
+                vectora3.clear();
+                vectorb1.clear();
+                vectorb2.clear();
+                vectorb3.clear();
+                _OrthogonalVectorsState();
+              });
+            }),
+            const SizedBox(height: 25),
+            const Divider(
+              color: Color.fromARGB(70, 200, 0, 0),
+              thickness: 3,
+              indent: 5,
+              endIndent: 5,
+            ),
+            const SizedBox(height: 10),
+            ansRow("Orthogonal : $ans"),
+            const SizedBox(height: 20),
+          ],
+        );
+    }
+  }
+
+  int dimflag = 1;
+  double a1 = 0;
+  double a2 = 0;
+  double a3 = 0;
+  double b1 = 0;
+  double b2 = 0;
+  double b3 = 0;
+  double dot = 0;
+  String ans = "";
 }
--- lib/screens/vector_spaces/projection/projection.dart
+++ lib/screens/vector_spaces/projection/projection.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:linear_algebra/main.dart';
 
@@ -8,6 +10,13 @@ class Projection extends StatefulWidget {
   State<Projection> createState() => _ProjectionState();
 }
 
+TextEditingController vectora1 = TextEditingController();
+TextEditingController vectora2 = TextEditingController();
+TextEditingController vectora3 = TextEditingController();
+TextEditingController vectorb1 = TextEditingController();
+TextEditingController vectorb2 = TextEditingController();
+TextEditingController vectorb3 = TextEditingController();
+
 class _ProjectionState extends State<Projection> {
   @override
   Widget build(BuildContext context) {
@@ -16,11 +25,421 @@ class _ProjectionState extends State<Projection> {
         appBar: appbar("Projection"),
         body: Container(
           decoration: background(),
-          child: const Center(
-              child: Text("Projection",
-                  style: TextStyle(fontSize: 50, color: Colors.red))),
+          child: ListView(
+            padding:
+                const EdgeInsets.only(top: 25, right: 10, bottom: 10, left: 10),
+            children: [
+              Card(
+                  shadowColor: Colors.red, elevation: 10, child: vectorColumn())
+            ],
+          ),
         ),
       ),
     );
   }
+
+  vectorColumn() {
+    switch (dimflag) {
+      case 1:
+        return Column(
+          children: [
+            const SizedBox(height: 10),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text(
+                "Select Dimension : ",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(width: 5),
+              redTextButton("1D", () {
+                setState(() {
+                  dimflag = 1;
+                  cAB1 = 0;
+                  cAB2 = 0;
+                  cAB3 = 0;
+                  cBA1 = 0;
+                  cBA2 = 0;
+                  cBA3 = 0;
+                  _ProjectionState();
+                });
+              }),
+              const SizedBox(width: 10),
+              whiteTextButton("2D", () {
+                setState(() {
+                  dimflag = 2;
+                  cAB1 = 0;
+                  cAB2 = 0;
+                  cAB3 = 0;
+                  cBA1 = 0;
+                  cBA2 = 0;
+                  cBA3 = 0;
+                  _ProjectionState();
+                });
+              }),
+              const SizedBox(width: 10),
+              whiteTextButton("3D", () {
+                setState(() {
+                  dimflag = 3;
+                  cAB1 = 0;
+                  cAB2 = 0;
+                  cAB3 = 0;
+                  cBA1 = 0;
+                  cBA2 = 0;
+                  cBA3 = 0;
+                  _ProjectionState();
+                });
+              })
+            ]),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Vector A : ",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(width: 15),
+                SizedBox(
+                  child: texInput(100, 15, vectora1, "a1"),
+                )
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Vector B : ",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(width: 15),
+                SizedBox(
+                  child: texInput(100, 15, vectorb1, "b1"),
+                )
+              ],
+            ),
+            const SizedBox(height: 25),
+            whiteTextButton('Calculate', () {
+              setState(() {
+                a1 = double.parse(vectora1.text);
+                b1 = double.parse(vectorb1.text);
+                dot = (a1 * b1);
+                magnitudeA = a1;
+                magnitudeB = b1;
+                projofAonB = (dot / (magnitudeB * magnitudeB));
+                projofBonA = (dot / (magnitudeA * magnitudeA));
+                cAB1 = projofAonB * b1;
+                cBA1 = projofBonA * a1;
+                vectora1.clear();
+                vectorb1.clear();
+                _ProjectionState();
+              });
+            }),
+            const SizedBox(height: 25),
+            const Divider(
+              color: Color.fromARGB(70, 200, 0, 0),
+              thickness: 3,
+              indent: 5,
+              endIndent: 5,
+            ),
+            const SizedBox(height: 10),
+            ansRow("Projection of A on B :"),
+            ansRow("     i = $cAB1"),
+            const SizedBox(height: 5),
+            ansRow("Projection of B on A :"),
+            ansRow("     i = $cBA1"),
+            const SizedBox(height: 20),
+          ],
+        );
+
+      case 2:
+        return Column(
+          children: [
+            const SizedBox(height: 10),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text(
+                "Select Dimension : ",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(width: 5),
+              whiteTextButton("1D", () {
+                setState(() {
+                  dimflag = 1;
+                  cAB1 = 0;
+                  cAB2 = 0;
+                  cAB3 = 0;
+                  cBA1 = 0;
+                  cBA2 = 0;
+                  cBA3 = 0;
+                  _ProjectionState();
+                });
+              }),
+              const SizedBox(width: 10),
+              redTextButton("2D", () {
+                setState(() {
+                  dimflag = 2;
+                  cAB1 = 0;
+                  cAB2 = 0;
+                  cAB3 = 0;
+                  cBA1 = 0;
+                  cBA2 = 0;
+                  cBA3 = 0;
+                  _ProjectionState();
+                });
+              }),
+              const SizedBox(width: 10),
+              whiteTextButton("3D", () {
+                setState(() {
+                  dimflag = 3;
+                  cAB1 = 0;
+                  cAB2 = 0;
+                  cAB3 = 0;
+                  cBA1 = 0;
+                  cBA2 = 0;
+                  cBA3 = 0;
+                  _ProjectionState();
+                });
+              })
+            ]),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Vector A : ",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(width: 15),
+                SizedBox(
+                  child: texInput(80, 15, vectora1, "a1"),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  child: texInput(80, 15, vectora2, "a2"),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Vector B : ",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(width: 15),
+                SizedBox(
+                  child: texInput(80, 15, vectorb1, "b1"),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  child: texInput(80, 15, vectorb2, "b2"),
+                ),
+              ],
+            ),
+            const SizedBox(height: 25),
+            whiteTextButton('Calculate', () {
+              setState(() {
+                a1 = double.parse(vectora1.text);
+                a2 = double.parse(vectora2.text);
+                b1 = double.parse(vectorb1.text);
+                b2 = double.parse(vectorb2.text);
+                dot = (a1 * b1) + (a2 * b2);
+                magnitudeA = sqrt((a1 * a1) + (a2 * a2));
+                magnitudeB = sqrt((b1 * b1) + (b2 * b2));
+                projofAonB = (dot / (magnitudeB * magnitudeB));
+                projofBonA = (dot / (magnitudeA * magnitudeA));
+                cAB1 = projofAonB * b1;
+                cAB2 = projofAonB * b2;
+                cBA1 = projofBonA * a1;
+                cBA2 = projofBonA * a2;
+                vectora1.clear();
+                vectora2.clear();
+                vectorb1.clear();
+                vectorb2.clear();
+                _ProjectionState();
+              });
+            }),
+            const SizedBox(height: 25),
+            const Divider(
+              color: Color.fromARGB(70, 200, 0, 0),
+              thickness: 3,
+              indent: 5,
+              endIndent: 5,
+            ),
+            const SizedBox(height: 10),
+            ansRow("Projection of A on B :"),
+            ansRow("     i = $cAB1"),
+            ansRow("     j = $cAB2"),
+            const SizedBox(height: 5),
+            ansRow("Projection of B on A :"),
+            ansRow("     i = $cBA1"),
+            ansRow("     j = $cBA2"),
+            const SizedBox(height: 20),
+          ],
+        );
+
+      case 3:
+        return Column(
+          children: [
+            const SizedBox(height: 10),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text(
+                "Select Dimension : ",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(width: 5),
+              whiteTextButton("1D", () {
+                setState(() {
+                  dimflag = 1;
+                  cAB1 = 0;
+                  cAB2 = 0;
+                  cAB3 = 0;
+                  cBA1 = 0;
+                  cBA2 = 0;
+                  cBA3 = 0;
+                  _ProjectionState();
+                });
+              }),
+              const SizedBox(width: 10),
+              whiteTextButton("2D", () {
+                setState(() {
+                  dimflag = 2;
+                  cAB1 = 0;
+                  cAB2 = 0;
+                  cAB3 = 0;
+                  cBA1 = 0;
+                  cBA2 = 0;
+                  cBA3 = 0;
+                  _ProjectionState();
+                });
+              }),
+              const SizedBox(width: 10),
+              redTextButton("3D", () {
+                setState(() {
+                  dimflag = 3;
+                  cAB1 = 0;
+                  cAB2 = 0;
+                  cAB3 = 0;
+                  cBA1 = 0;
+                  cBA2 = 0;
+                  cBA3 = 0;
+                  _ProjectionState();
+                });
+              })
+            ]),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Vector A : ",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(width: 15),
+                SizedBox(
+                  child: texInput(60, 15, vectora1, "a1"),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  child: texInput(60, 15, vectora2, "a2"),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  child: texInput(60, 15, vectora3, "a3"),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Vector B : ",
+                  style: TextStyle(fontSize: 15),
+                ),
+                const SizedBox(width: 15),
+                SizedBox(
+                  child: texInput(60, 15, vectorb1, "b1"),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  child: texInput(60, 15, vectorb2, "b2"),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  child: texInput(60, 15, vectorb3, "b2"),
+                ),
+              ],
+            ),
+            const SizedBox(height: 25),
+            whiteTextButton('Calculate', () {
+              setState(() {
+                a1 = double.parse(vectora1.text);
+                a2 = double.parse(vectora2.text);
+                a3 = double.parse(vectora3.text);
+                b1 = double.parse(vectorb1.text);
+                b2 = double.parse(vectorb2.text);
+                b3 = double.parse(vectorb3.text);
+                dot = (a1 * b1) + (a2 * b2) + (a3 * b3);
+                magnitudeA = sqrt((a1 * a1) + (a2 * a2) + (a3 * a3));
+                magnitudeB = sqrt((b1 * b1) + (b2 * b2) + (b3 * b3));
+                projofAonB = (dot / (magnitudeB * magnitudeB));
+                projofBonA = (dot / (magnitudeA * magnitudeA));
+                cAB1 = projofAonB * b1;
+                cAB2 = projofAonB * b2;
+                cAB3 = projofAonB * b3;
+                cBA1 = projofBonA * a1;
+                cBA2 = projofBonA * a2;
+                cBA3 = projofBonA * a3;
+                vectora1.clear();
+                vectora2.clear();
+                vectora3.clear();
+                vectorb1.clear();
+                vectorb2.clear();
+                vectorb3.clear();
+                _ProjectionState();
+              });
+            }),
+            const SizedBox(height: 25),
+            const Divider(
+              color: Color.fromARGB(70, 200, 0, 0),
+              thickness: 3,
+              indent: 5,
+              endIndent: 5,
+            ),
+            const SizedBox(height: 10),
+            ansRow("Projection of A on B :"),
+            ansRow("     i = $cAB1"),
+            ansRow("     j = $cAB2"),
+            ansRow("     k = $cAB3"),
+            const SizedBox(height: 5),
+            ansRow("Projection of B on A :"),
+            ansRow("     i = $cBA1"),
+            ansRow("     j = $cBA2"),
+            ansRow("     k = $cBA3"),
+            const SizedBox(height: 20),
+          ],
+        );
+    }
+  }
+
+  int dimflag = 1;
+  double a1 = 0;
+  double a2 = 0;
+  double a3 = 0;
+  double b1 = 0;
+  double b2 = 0;
+  double b3 = 0;
+  double dot = 0;
+  double magnitudeA = 0;
+  double magnitudeB = 0;
+  double projofAonB = 0;
+  double projofBonA = 0;
+  double cAB1 = 0;
+  double cAB2 = 0;
+  double cAB3 = 0;
+  double cBA1 = 0;
+  double cBA2 = 0;
+  double cBA3 = 0;
+  double angle = 0;
 }
