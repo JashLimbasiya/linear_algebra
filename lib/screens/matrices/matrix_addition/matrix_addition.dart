8beb8458867f11ca2edb9f011d77ec9ad959c893
JashLimbasiya11 <89210135+JashLimbasiya@users.noreply.github.com>
2023-10-21 14:09:07 +0530

    21_10_2023

--- lib/screens/matrices/matrix_addition/matrix_addition.dart
+++ lib/screens/matrices/matrix_addition/matrix_addition.dart
@@ -9,6 +9,16 @@ class MatrixAddition extends StatefulWidget {
 }
 
 class _MatrixAdditionState extends State<MatrixAddition> {
+  var lists = [1, 2, 3, 4, 5];
+  int row = 2;
+  int column = 2;
+  List<List<int>> matrixA = [];
+  List<List<int>> matrixB = [];
+  // // ignore: prefer_typing_uninitialized_variables
+  // var twoDList1;
+  // // ignore: prefer_typing_uninitialized_variables
+  // var twoDList2;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -16,9 +26,86 @@ class _MatrixAdditionState extends State<MatrixAddition> {
         appBar: appbar("Matrix Addition"),
         body: Container(
           decoration: background(),
-          child: const Center(
-              child: Text("Matrix Addition",
-                  style: TextStyle(fontSize: 50, color: Colors.red))),
+          child: ListView(
+            padding:
+                const EdgeInsets.only(top: 25, right: 10, bottom: 10, left: 10),
+            children: [
+              Card(
+                shadowColor: Colors.red,
+                elevation: 10,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Raw : ",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        DropdownButton(
+                          value: row,
+                          icon: const Icon(Icons.keyboard_arrow_down),
+                          items: lists.map((int items) {
+                            return DropdownMenuItem(
+                              value: items,
+                              child: Text(
+                                "$items",
+                                style: const TextStyle(fontSize: 20),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (int? newValue) {
+                            setState(() {
+                              row = newValue!;
+                            });
+                          },
+                        ),
+                        const SizedBox(width: 10),
+                        const Text(
+                          "Column : ",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        DropdownButton(
+                          value: column,
+                          icon: const Icon(Icons.keyboard_arrow_down),
+                          items: lists.map((int items) {
+                            return DropdownMenuItem(
+                              value: items,
+                              child: Text(
+                                "$items",
+                                style: const TextStyle(fontSize: 20),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (int? newValue) {
+                            setState(() {
+                              column = newValue!;
+                              // twoDList1 = List.generate(row, (i) => List.generate(column, (j) => i+j));
+                              // twoDList2 = List<List>.generate(row, (i) => List<dynamic>.generate(column, (index) => null, growable: false), growable: false);
+                              // twoDList1[1][1] = "Jash";
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    for (int i = 0; i < row; i++)
+                      Row(
+                        children: [
+                          for (int j = 0; j < column; j++)
+                            Column(
+                              children: [whiteTextButton("str", () {})],
+                            )
+                        ],
+                      ),
+                    const SizedBox(height: 10),
+                    textRow("$row"),
+                    textRow("$column"),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
